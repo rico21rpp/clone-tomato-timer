d095efd99df59414302d546c31f8d267c29fda63
rico.putra <rico.putra@outlook.co.id>
2020-05-11 04:49:57 +0700

    update counter text timely

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // set global variable
 var timer;
-var minutes = 1;
+var minutes = 25;
 var seconds = 0;
 var isTimerOn = false;
+
+var minuteStr = "25";
+var secondStr = "00";
+
+var counter = document.querySelector('#counter');
 
 const btnPomodoro = document.querySelector('.btn__timer--pomodoro');
 const btnShortBreak = document.querySelector('.btn__timer--short');
@@ -12,22 +17,38 @@ const btnStart = document.querySelector('.btn__action--start');
 const btnStop = document.querySelector('.btn__action--stop');
 const btnReset = document.querySelector('.btn__action--reset');
 
+const setCounter = () => {
+    minuteStr = minutes.toString();
+    setSecondStr();
+    counter.innerHTML = `${minuteStr}:${secondStr}`;
+}
+
+function setSecondStr() {
+    if (seconds < 10) {
+        secondStr = `0${seconds}`;
+    }
+    else {
+        secondStr = seconds.toString();
+    }
+}
+
 const startTimer = () => {
     if (isTimerOn == false) {
         isTimerOn = true;
         timer = setInterval(function() {
             if (minutes > 0 && seconds == 0) {
                 minutes--;
-                seconds = 59;
+                seconds = 60;
             }
             else if (minutes == 0 && seconds == 0) {
                 clearInterval(timer);
                 isTimerOn = false;
                 minutes = 25;
+                setCounter();
                 return;
             }
             seconds--;
-            console.log({minutes, seconds});
+            setCounter();
         }, 1000);
     }
 }
@@ -43,12 +64,9 @@ const resetTimer = () => {
     isTimerOn = false;
     minutes = 25;
     seconds = 0;
+    setCounter();
 }
 
 btnStart.addEventListener('click', startTimer);
 btnStop.addEventListener('click', stopTimer);
 btnReset.addEventListener('click', resetTimer);
-
-// console.log(btnPomodoro);
-// console.log(btnShortBreak);
-// console.log(btnLongBreak);
