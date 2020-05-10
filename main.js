2004500c9f1a97e31ea8f3c0da4c121f25fa1bae
rico.putra <rico.putra@outlook.co.id>
2020-05-10 10:25:52 +0700

    prepare variables in js

---
+++ main.js
@@ -0,0 +1,13 @@
+// set global variable
+let minutes = 25;
+let seconds = 0;
+let isTimerOn = false;
+
+const btnPomodoro = document.querySelector('.btn__timer--pomodoro');
+const btnShortBreak = document.querySelector('.btn__timer--short');
+const btnLongBreak = document.querySelector('.btn__timer--long');
+
+const btnStart = document.querySelector('.btn__action--start');
+const btnStop = document.querySelector('.btn__action--stop');
+const btnReset = document.querySelector('.btn__action--reset');
+
